7a369fbab189fcf08591dd5e6493fedcc7d500ad
kisia0916 <fumiharuabe@gmail.com>
2025-08-24 22:18:36 +0900

    add analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -50,6 +51,7 @@ export default function RootLayout({
       >
         {/* <SmoothScroller /> */}
         {children}
+        <Analytics/>
       </body>
     </html>
   );
